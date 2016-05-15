8f3e865ebea78b67a78fb6b01b489038daf96afd
Elsis-Sitati <sitatielsis@gmail.com>
2016-05-15 11:24:06 +0300

    added script to run app

---
+++ config.py
@@ -0,0 +1,4 @@
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
---
+++ manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from flask.ext.script import Manager
+
+
+manager = Manager(app)
+
+if __name__ == '__main__':
+manager.run()
+

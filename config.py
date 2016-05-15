a89eea2392c5882ffdca16e73b6493c8a14b6490
Elsis-Sitati <sitatielsis@gmail.com>
2016-05-15 15:59:47 +0300

    edited configurations settings

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+	FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	
+	@staticmethod
+	def init_app(app):
+		pass
+
